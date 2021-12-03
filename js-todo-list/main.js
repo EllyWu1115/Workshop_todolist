f20fc5eed6bf19f0caa7987f0d37f121903ffa23
elly <lian1115.lw@gmail.com>
2021-12-03 16:15:46 +0800

    add task_1

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,21 @@
 // TO DO
+document.addEventListener("DOMContentLoaded",() =>{
+  // const colseBtn = document.querySelector(".close");
+  const ul = document.querySelector("ul");
+  // const addinput = document.querySelector("#input");
+  // const addBtn = document.querySelector(".addBtn");
+ 
+
+  ul.addEventListener("click",(e) =>{
+    // console.log(e.target);判斷有沒有抓到事件
+   if(e.target && e.target.className === "checked"){
+     e.target.classList.remove("checked");
+   }
+   else{
+     e.target.classList.add("checked")
+   }
+   
+  });
+})
+
+
